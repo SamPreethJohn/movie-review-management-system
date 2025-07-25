c2c6d6ba2bc09cb22037d0d7a7510d8da0e48c1f
SamPreethJohn <dcruzsam248@gmail.com>
2025-07-25 18:47:41 +0530

    Initial commit

---
+++ src/main/java/com/sam/movie/moviereview/MovieReviewManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sam.movie.moviereview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReviewManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReviewManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sam/movie/moviereview/MovieReviewManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.movie.moviereview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
